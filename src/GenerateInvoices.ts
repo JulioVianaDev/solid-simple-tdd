565aba4431554103b8780b42851c5e092113bd08
juliovianadev <juliovianadev@gmail.com>
2024-09-12 00:17:22 -0300

    Aula dto

--- src/GenerateInvoices.ts
+++ src/GenerateInvoices.ts
@@ -1,9 +1,42 @@
 import pg from "pg-promise"
-export class GenerateInvoices{
-    async execute():Promise<any>{
-        const connection = await  pg()("postgres://julio:tomate@localhost:5432/julio")
-        const contracts = await connection.query('select * from julio.contract',[]);
-        console.log(contracts);
-        return []
+import moment from "moment";
+export class GenerateInvoices {
+    async execute(input: Input): Promise<Output[]> {
+        const connection = await pg()("postgres://julio:tomate@localhost:5432/julio")
+        const contracts = await connection.query('select * from julio.contract', []);
+        const output: Output[] = []
+        for (const contract of contracts) {
+            if (input.type === "cash") {
+
+
+                const payments = await connection.query('select * from julio.payment where id_contract = $1', [contract.id_contract])
+                for (const payment of payments) {
+                    if (payment.date.getMonth() + 1 !== input.month || payment.date.getFullYear() !== input.year) continue;
+                    output.push({ date: moment(payment.date).format("YYYY-MM-DD"), amount: +payment.amount })
+                }
+            }
+            if (input.type === "accrual") {
+                let period = 0;
+                while (period <= contract.periods) {
+                    const date = moment(contract.date).add(period++, "months").toDate();
+                    if (date.getMonth() + 1 !== input.month || date.getFullYear() !== input.year) continue
+                    const amount = parseFloat(contract.amount) / contract.periods;
+                    output.push({ date: moment(date).format("YYYY-MM-DD"), amount })
+                }
+            }
+        }
+        await connection.$pool.end();
+        return output
     }
+}
+
+export type Input = {
+    month: number,
+    year: number,
+    type: "accrual" | "cash"
+}
+
+export type Output = {
+    date: string,
+    amount: number
 }
--- test/GenerateInvoices.test.ts
+++ test/GenerateInvoices.test.ts
@@ -1,14 +1,40 @@
-import { GenerateInvoices } from "../src/GenerateInvoices";
+import { GenerateInvoices, Input } from "../src/GenerateInvoices";
 
-test("Deve gerar as notas fiscais", async () => {
+test("Deve gerar as notas fiscais por regime de caixa", async () => {
     const generateInvoices = new GenerateInvoices();
-    const input = {
+    const input: Input = {
         month: 1,
         year: 2022,
-        type: "cash" // "accrual" | "cash" 
+        type: "cash" // "accrual" | "cash"
     }
 
-    const output = await generateInvoices.execute(input)
-    expect(output.at(0)?.date).toBe("2022-01-05");
-    expect(output.at(0)?.date).toBe(6000)
+    const output = await generateInvoices.execute(input);
+    expect(output[0].date).toBe("2022-01-05");
+    expect(output[0].amount).toBe(6000)
+})
+
+
+test("Deve gerar as notas fiscais por regime de competencia 1 mes", async () => {
+    const generateInvoices = new GenerateInvoices();
+    const input: Input = {
+        month: 1,
+        year: 2022,
+        type: "accrual" // "accrual" | "cash"
+    }
+
+    const output = await generateInvoices.execute(input);
+    expect(output[0].date).toBe("2022-01-01");
+    expect(output[0].amount).toBe(500)
+})
+test("Deve gerar as notas fiscais por regime de competencia 2 mes", async () => {
+    const generateInvoices = new GenerateInvoices();
+    const input: Input = {
+        month: 2,
+        year: 2022,
+        type: "accrual" // "accrual" | "cash"
+    }
+
+    const output = await generateInvoices.execute(input);
+    expect(output[0].date).toBe("2022-02-01");
+    expect(output[0].amount).toBe(500)
 })
